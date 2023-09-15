c496df9ae30c1877ef086f2fad8f0bc4572878be
adarsh777 <adarshnishad777@gmail.com>
2023-09-15 12:01:52 -0500

    Initail commit

---
+++ CRUD Operation/Migrations/20230915020938_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD_Operation.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    firstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    lastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    dateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MobileNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfo", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+        }
+    }
+}
---
+++ CRUD Operation/Models/UserInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_OPERATION.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string? firstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string? lastName { get; set; }
+
+        [Required]
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime dateOfBirth { get; set; }
+
+
+        [Display(Name = "Mobile Number")]
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [RegularExpression("^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
+        public string MobileNo { get; set; }
+
+
+        [Required]
+        [Display(Name = "City")]
+        public string? City { get; set; }
+    }
+}
