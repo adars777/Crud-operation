b879239dac6d9b0b32be3be1d1c6d91fc43f61df
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2024-01-31 17:59:31 +0530

    Add files via upload

---
+++ Controllers/UserInfoController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUD_OPERATION.Models;
+using CRUD_Operation.Data;
+
+namespace CRUD_Operation.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        private readonly CRUD_OperationContext _context;
+
+        public UserInfoController(CRUD_OperationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserInfoes
+        public async Task<IActionResult> Index()
+        {
+              return _context.UserInfo != null ? 
+                          View(await _context.UserInfo.ToListAsync()) :
+                          Problem("Entity set 'CRUD_OperationContext.UserInfo'  is null.");
+        }
+
+        // GET: UserInfoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.UserInfo == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.UserInfo
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserInfoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,firstName,lastName,dateOfBirth,MobileNo,City")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userInfo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.UserInfo == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.UserInfo.FindAsync(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,firstName,lastName,dateOfBirth,MobileNo,City")] UserInfo userInfo)
+        {
+            if (id != userInfo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userInfo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserInfoExists(userInfo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.UserInfo == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.UserInfo
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.UserInfo == null)
+            {
+                return Problem("Entity set 'CRUD_OperationContext.UserInfo'  is null.");
+            }
+            var userInfo = await _context.UserInfo.FindAsync(id);
+            if (userInfo != null)
+            {
+                _context.UserInfo.Remove(userInfo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserInfoExists(int id)
+        {
+          return (_context.UserInfo?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Data/CRUD_OperationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUD_OPERATION.Models;
+
+namespace CRUD_Operation.Data
+{
+    public class CRUD_OperationContext : DbContext
+    {
+        public CRUD_OperationContext (DbContextOptions<CRUD_OperationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUD_OPERATION.Models.UserInfo> UserInfo { get; set; } = default!;
+    }
+}
